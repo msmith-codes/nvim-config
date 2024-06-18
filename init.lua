0e5145edaad743e257a3fa5bb30e64ae814a7aec
GSRS - Work <gsrs-work@Michaels-Mac-mini.local>
2024-06-18 08:44:19 -0400

    fixed go lsp:

--- init.lua
+++ init.lua
@@ -2,3 +2,22 @@ require("config.options")
 require("config.mappings")
 require("config.autocmds")
 require("config.lazy")
+
+-- General/Global LSP Configuration
+local api = vim.api
+local lsp = vim.lsp
+
+local make_client_capabilities = lsp.protocol.make_client_capabilities
+function lsp.protocol.make_client_capabilities()
+    local caps = make_client_capabilities()
+    if not (caps.workspace or {}).didChangeWatchedFiles then
+        vim.notify(
+            'lsp capability didChangeWatchedFiles is already disabled',
+            vim.log.levels.WARN
+        )
+    else
+        caps.workspace.didChangeWatchedFiles = nil
+    end
+
+    return caps
+end
