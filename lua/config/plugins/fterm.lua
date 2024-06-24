a83bca80d57ce0cf8a9c806ba6e6f5878070a99c
msmith-codes <info@msmith.codes>
2024-06-24 08:52:28 -0400

    Added floating term

---
+++ lua/config/plugins/fterm.lua
@@ -0,0 +1,10 @@
+require('FTerm').setup({
+    dimensions = {
+        height = 0.9,
+        width = 0.9,
+    }
+})
+
+vim.keymap.set('n', '<C-i>', '<CMD>lua require("FTerm").toggle()<CR>')
+vim.keymap.set('t', '<C-i>', '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>')
+
